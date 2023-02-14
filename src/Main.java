b6e746acf4e32055154d54513a20ca9f1487cb82
Nikita Goryachev <ackerman_x@mail.ru>
2023-02-14 20:13:28 +0300

    task_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,71 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+Текстовый файл содержит следующую информацию о персонах (Person) (имя, фамилия, пол, возраст).
+Путь к файлу должен задаваться через консоль.
+Каждое поле в файле разделено запятой.
+Написать программу, которая читает информации из входного файла, сортирует персонов по имени и фамилии.
+Отсортированные данные вывести в тот же файл, перезаписав его.
+На консоль вывести количество человек с возрастом > 30.
+На консоль вывести количество мужчин и женщин.
+
+Пример данных в исходном файле:
+Jack,Ward,male,16
+Alex,Wilson, male,29
+Monica,Lopez, female,33
+
+Пример вывода на консоль:
+Count: 1
+male:2
+female:1
+
+Note:
+Для работы с файлами можно использовать класс File.
+Организовать корректую обработку исключений.
+Для сортировки и вычислений можно использовать Stream.
+*/
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        List<Person> people = new ArrayList<>();
+        int ageCount;
+        int maleCount;
+        int femaleCount;
+        System.out.println("Введите ваш путь к файлу");
+        String path = new Scanner(System.in).nextLine();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String document;
+            while ((document = br.readLine()) != null) {
+                String[] infPerson = document.split(",");
+                people.add(new Person(infPerson[0].trim(), infPerson[1].trim(),
+                        infPerson[2].trim(), Integer.parseInt(infPerson[3].trim())));
+                System.out.println(document);
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.println("Не верный путь!");
+        }
+        people.sort(Comparator.comparing(p -> (p.getFirstName() + p.getLastName())));
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            for (Person person : people) {
+                bw.write(person.getFirstName() + "," + person.getLastName() + "," +
+                        person.getSex() + "," + person.getAge() + "\n");
+            }
+        }
+        ageCount = (int) people.stream().filter(age -> age.getAge() > 30).count();
+        System.out.println();
+        System.out.println("Count: " + ageCount);
+        maleCount = (int) people.stream().filter(male -> male.getSex().trim().equals("male")).count();
+        System.out.println("Male: " + maleCount);
+        femaleCount = (int) people.stream().filter(female -> female.getSex().trim().equals("female")).count();
+        System.out.println("Female: " + femaleCount);
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    public String firstName;
+    public String lastName;
+    public String sex;
+    public int age;
+
+    public Person(String firstName, String lastName, String sex, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
